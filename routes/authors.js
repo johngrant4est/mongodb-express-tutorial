4549490346557765a5aa92fd9a869bb7e1bb9d8a
J Grant Forrest <johngrant4est@users.noreply.github.com>
2021-10-27 23:20:31 +0100

    Finished Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
-//All authors route
+// All authors route
 router.get('/', async(req, res) => {
     let searchOptions = {}
     if (req.query.name != null && req.query.name !== '') {
@@ -33,13 +34,77 @@ router.post('/', async(req, res) => {
 
     try {
         const newAuthor = await author.save()
-            // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect(`authors`)
+        res.redirect(`authors/${newAuthor.id}`)
+
     } catch (err) {
         res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating Author'
         })
+    }
+})
+
+// Show author
+router.get('/:id', async(req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        const books = await Book.find({ author: author.id }).limit(6).exec()
+        res.render('authors/show', {
+            author: author,
+            booksByAuthor: books
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+// Edit Author
+router.get('/:id/edit', async(req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', { author: author });
+
+    } catch {
+        res.redirect('/authors')
+    }
+
+})
+
+router.put('/:id', async(req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch (err) {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect('/authors')
+    } catch (err) {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
+
     }
 })
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -14,8 +15,10 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10Mb', extended: false }))
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
